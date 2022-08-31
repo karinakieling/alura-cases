9baf1618312cac4d5deb12881ca39e9e15713b6e
Karina <karinakielingtondin@gmail.com>
2022-08-30 21:00:31 -0300

    first commit

---
+++ pages/404.jsx
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function NotFound() {
+    return(
+        <div>
+        <h1>404 - Parece que você se perdeu!</h1>
+        <Link href="/">Voltar para a Página inicial</Link>
+      </div>
+    )
+}
